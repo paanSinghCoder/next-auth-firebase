8bd6c779fc1e1560968e3e6a1abc6a641da86c0a
paanSinghCoder <gaurav3017@gmail.com>
2024-02-25 13:30:32 +0530

    add space

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <>
       <div className="mx-auto max-w-6xl flex flex-col items-center justify-center min-h-[300px]">
-        <h1 className="text-8xl font-bold opacity-80 mt-12">
+        <h1 className="text-8xl font-bold opacity-80 mt-12 px-3">
           Next-Auth with Firebase
         </h1>
-        <p className="opacity-50 text-xl font-normal mt-8">
+        <p className="opacity-50 text-xl font-normal mt-8 px-3">
           This is a public landing page. /dashboard is protected. Go to /login
           to register or login.
         </p>
